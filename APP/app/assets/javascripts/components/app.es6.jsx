3aec83546d2f1adf53cd244809d882a09f4cd6cd
levytsroman <r.levyts@gmail.com>
2017-02-12 00:16:19 -0500

    extract sectors into components

--- APP/app/assets/javascripts/components/app.es6.jsx
+++ APP/app/assets/javascripts/components/app.es6.jsx
@@ -1,28 +1,19 @@
 class App extends React.Component {
   render(){
+    let games = ["fa fa-bell-o", "fa fa-comment-o", "fa fa-smile-o", "fa fa-heart-o", "fa fa-star-o", "fa fa-lightbulb-o"]
     return(
       <div id="wrapper">
+        <div id="wheel">
+          <div id="inner-wheel">
+            {games.map( (game, i) => <Sector data={game} key={i}/>)}
+          </div>
+          <div id="spin">
+              <div id="inner-spin"></div>
+          </div>
 
-            <div id="wheel">
-
-                <div id="inner-wheel">
-                    <div className="sec"><span className="fa fa-bell-o"></span></div>
-                    <div className="sec"><span className="fa fa-comment-o"></span></div>
-                    <div className="sec"><span className="fa fa-smile-o"></span></div>
-                    <div className="sec"><span className="fa fa-heart-o"></span></div>
-                    <div className="sec"><span className="fa fa-star-o"></span></div>
-                    <div className="sec"><span className="fa fa-lightbulb-o"></span></div>
-                </div>
-
-                <div id="spin">
-                    <div id="inner-spin"></div>
-                </div>
-
-                <div id="shine"></div>
-            </div>
-
-
-            <div id="txt"></div>
+          <div id="shine">Shine</div>
+        </div>
+        <div id="txt">Hello</div>
       </div>
     )
   }
---
+++ APP/app/assets/javascripts/components/sector.es6.jsx
@@ -0,0 +1,7 @@
+class Sector extends React.Component {
+  render(){
+    return(
+      <div className="sec"><span className={this.props.data}></span></div>
+    )
+  }
+}
