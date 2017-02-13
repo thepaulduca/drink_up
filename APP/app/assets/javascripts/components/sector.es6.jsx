4173a7a740bf1f7e6e463d53922637621c2d5482
Paul Duca <paul.r.duca@gmail.com>
2017-02-13 12:41:18 -0500

    spacing

--- APP/app/assets/javascripts/components/sector.es6.jsx
+++ APP/app/assets/javascripts/components/sector.es6.jsx
@@ -1,7 +1,9 @@
 class Sector extends React.Component {
   render(){
     return(
-      <div name={this.props.data.gameName} className="sec"><span className={this.props.data.gameIcon}></span></div>
+      <div name={this.props.data.gameName} className="sec">
+        <span className={this.props.data.gameIcon}></span>
+      </div>
     )
   }
 }
