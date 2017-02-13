3ba1b5fc3416512db8c93bb5a0a5e8cd8d5244db
Paul Duca <paul.r.duca@gmail.com>
2017-02-13 16:36:01 -0500

    remove jQuery

--- APP/app/assets/javascripts/components/app.es6.jsx
+++ APP/app/assets/javascripts/components/app.es6.jsx
@@ -3,7 +3,6 @@ class App extends React.Component {
     return(
       <div id="wrapper">
             <Wheel />
-            <div id="txt"></div>
       </div>
     )
   }
--- APP/app/assets/javascripts/components/wheel.es6.jsx
+++ APP/app/assets/javascripts/components/wheel.es6.jsx
@@ -1,9 +1,30 @@
 class Wheel extends React.Component {
+
+constructor(props) {
+   super(props);
+   this.state = {
+     clicks: 0,
+     degree: 1800
+   };
+   this.handleSpin = this.handleSpin.bind(this);
+ }
+
+ handleSpin() {
+   this.state.clicks ++;
+   clicks = this.state.clicks;
+   var degree = (1800 * clicks) + (Math.floor(Math.random() * 360)+ 1);
+   var innerWheel = document.getElementById("inner-wheel")
+   innerWheel.style.transform = 'rotate(' + degree + 'deg)'
+ }
+
+
   render(){
     return(
       <div id="wheel">
           <InnerWheel/>
+        <div onClick={this.handleSpin}>
           <Spinner/>
+        </div>
       </div>
     )
   }
