bb2a71e77e96201dc68966b23192ecbe5575fc77
levytsroman <r.levyts@gmail.com>
2017-02-13 10:35:10 -0500

    add conditional rendering based on whether a game is chosen

--- APP/app/assets/javascripts/components/app.es6.jsx
+++ APP/app/assets/javascripts/components/app.es6.jsx
@@ -1,10 +1,30 @@
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      activeGame: null
+    }
+  }
+
+  activeGameHandler(newGame){
+    this.setState({
+      activeGame: newGame
+    })
+  }
+
   render(){
-    return(
-      <div id="wrapper">
-        <Wheel/>
-        <div id="txt"></div>
-      </div>
-    )
+    let activeGame = this.state.activeGame
+    if(activeGame){
+      return(
+        <div id="wrapper">
+          <Wheel/>
+          <div id="txt"></div>
+        </div>
+      )
+    } else {
+      return(
+        <Game game={activeGame}/>
+      )
+    }
   }
 }
