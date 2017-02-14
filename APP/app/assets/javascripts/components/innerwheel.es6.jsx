ec76400862e216bb3a001428da8ef5dfdf2e9a58
Paul Duca <paul.r.duca@gmail.com>
2017-02-14 14:57:17 -0500

    changes

--- APP/app/assets/javascripts/components/innerwheel.es6.jsx
+++ APP/app/assets/javascripts/components/innerwheel.es6.jsx
@@ -2,12 +2,12 @@ class InnerWheel extends React.Component {
   render(){
     return(
       <div id="inner-wheel">
-        <div className="sec"></div>
-        <div className="sec"></div>
-        <div className="sec"></div>
-        <div className="sec"></div>
-        <div className="sec"></div>
-        <div className="sec"></div>
+        <div className="sec"><span className="1"></span></div>
+        <div className="sec"><span className="2"></span></div>
+        <div className="sec"><span className="3"></span></div>
+        <div className="sec"><span className="4"></span></div>
+        <div className="sec"><span className="5"></span></div>
+        <div className="sec"><span className="6"></span></div>
       </div>
     )
   }
--- APP/app/assets/javascripts/components/spinner.es6.jsx
+++ APP/app/assets/javascripts/components/spinner.es6.jsx
@@ -2,7 +2,7 @@ class Spinner extends React.Component {
 
   render(){
     return(
-      <div id="spin" onClick={this.handleClick}>
+      <div id="spin">
         <div id="inner-spin"></div>
       </div>
     )
