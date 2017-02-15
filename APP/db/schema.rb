a1e2a6c73e351d1d53ac2468282b29e3ba762196
levytsroman <r.levyts@gmail.com>
2017-02-15 17:01:09 -0500

    update the database.yml with postgresql setup

--- APP/db/schema.rb
+++ APP/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170215192533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "trivia_answers", force: :cascade do |t|
     t.string   "answer"
     t.integer  "trivia_question_id"
