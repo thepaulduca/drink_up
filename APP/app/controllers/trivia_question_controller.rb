ea4023513256df4711e174a1ec5444ec0bd16437
levytsroman <r.levyts@gmail.com>
2017-02-16 23:53:26 -0500

    set up route that returns a trivia question and relevant answers

--- APP/app/controllers/trivia_question_controller.rb
+++ APP/app/controllers/trivia_question_controller.rb
@@ -1,3 +1,9 @@
 class TriviaQuestionController < ApplicationController
-  
+  def random_trivia_question
+    question = random_question
+    choices = question.answers
+    correct_choice = question.trivia_answer
+
+    render :json => {question: question, choices: choices, correctChoice: correct_choice}
+  end
 end
