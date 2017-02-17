a7df39ff856de7ebf9c1aa3bc154e5c805b12d1b
levytsroman <r.levyts@gmail.com>
2017-02-16 23:53:51 -0500

    build a helper method to get a random question

--- APP/app/controllers/application_controller.rb
+++ APP/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def random_question
+    # get random number between 1 and total number of questions
+    question_id = rand(1..TriviaQuestion.all.length)
+    TriviaQuestion.find_by(id: question_id)
+  end
 end
